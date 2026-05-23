6386545cc524317d0cb4bbf2e263e230ec97fb85
nexorareplyzenai-svg <nexora.replyzen.ai@gmail.com>
2026-05-24 01:32:04 +0200

    separated project into files

---
+++ sections.py
@@ -0,0 +1,33 @@
+def welcome():
+
+    name = input("What is your name? ")
+
+    while True:
+
+        age = input("How old are you? ")
+
+        if age.isdigit():
+            age = int(age)
+            break
+
+        else:
+            print("Please enter a number")
+
+    print("Welcome to Nexora,", name)
+
+    if age < 18:
+        print("You are young")
+
+    elif age < 60:
+        print("You are adult")
+
+    else:
+        print("You are old")
+
+
+def help_section():
+    print("This is the help section")
+
+
+def about_section():
+    print("Nexora Version 1.0")
